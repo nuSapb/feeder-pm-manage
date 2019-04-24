c5fddf843b71b7f4bad4284876e9f6f8e7432681
nuhundred@gmail.com <nuhundred@gmail.com>
2019-04-24 13:32:06 +0700

    edit a lot of code

--- src/route/controller/feedermanage/feeders.js
+++ src/route/controller/feedermanage/feeders.js
@@ -1,11 +1,11 @@
-const uuid4 = require('uuid4')
-const fs = require('fs-extra')
-const path = require('path')
+const uuid4 = require("uuid4")
+const fs = require("fs-extra")
+const path = require("path")
 
-const feeders = require('../../../models/feeders')
+const feeders = require("../../../models/feeders")
 
-const config = require('../../../config')
-const users = require('../../../models/user')
+const config = require("../../../config")
+const users = require("../../../models/user")
 
 const getHandler = async ctx => {
   console.log(ctx.session.username)
@@ -33,7 +33,8 @@ const getHandler = async ctx => {
     allFeeder: allFeeder,
     due: due,
     overDue: overDue,
-    onGoing, onGoing,
+    onGoing,
+    onGoing,
     scrap: scrap,
     repair: repair,
     size8Due: size8Due,
@@ -41,10 +42,10 @@ const getHandler = async ctx => {
     size16Due: size16Due,
     size24Due: size24Due,
     size32Due: size32Due,
-    otherSizeDue: otherSizeDue,
+    otherSizeDue: otherSizeDue
   }
 
-  await ctx.render('index', data)
+  await ctx.render("index", data)
 }
 
 const getData = async ctx => {
@@ -73,7 +74,8 @@ const getData = async ctx => {
     allFeeder: allFeeder,
     due: due,
     overDue: overDue,
-    onGoing, onGoing,
+    onGoing,
+    onGoing,
     scrap: scrap,
     repair: repair,
     size8Due: size8Due,
@@ -81,33 +83,33 @@ const getData = async ctx => {
     size16Due: size16Due,
     size24Due: size24Due,
     size32Due: size32Due,
-    otherSizeDue: otherSizeDue,
+    otherSizeDue: otherSizeDue
   }
 
   ctx.body = data
 }
 
-const listAllFeederDue = async (ctx) => {
+const listAllFeederDue = async ctx => {
   let data = {}
   const listAllDue = await feeders.findAllDue()
   console.log(listAllDue)
   data = {
-    listAllDue: listAllDue,
+    listAllDue: listAllDue
   }
-  await ctx.render('tables_dynamics', data)
+  await ctx.render("tables_dynamics", data)
 }
 
-const listAllOnhands = async (ctx) => {
+const listAllOnhands = async ctx => {
   let data = {}
   const listAllDue = await feeders.findAllDue()
   console.log(listAllDue)
   data = {
-    listAllDue: listAllDue,
+    listAllDue: listAllDue
   }
-  await ctx.render('tables_dynamics', data)
+  await ctx.render("tables_dynamics", data)
 }
 
-const findAllFeeder = async (ctx) => {
+const findAllFeeder = async ctx => {
   let data = {}
   const listAllFeeder = await feeders.findAll()
   const listAllOverDue = await feeders.findOverDue()
@@ -120,152 +122,200 @@ const findAllFeeder = async (ctx) => {
     listAllRepair: listAllRepair,
     listAllScrap: listAllScrap
   }
-  await ctx.render('tables', data)
+  await ctx.render("tables", data)
 }
 
-const findlastTenRows = async (ctx) => {
+const findlastTenRows = async ctx => {
   let data = {}
   const last10Rows = await feeders.findLast30Rows()
   console.log(last10Rows)
   ctx.session.flash = {
-    success: 'add feeder done'
+    success: "add feeder done"
   }
   data = {
     last10Rows: last10Rows,
     flash: ctx.flash
   }
-  await ctx.render('form', data)
+  await ctx.render("form", data)
 }
 
-const editHandler = async (ctx) => {
+const editHandler = async ctx => {
   let data = {}
   const listAllFeeder = await feeders.findAll()
   console.log(listAllFeeder)
 
   ctx.session.flash = {
-    success: 'edit feeder done'
+    success: "edit feeder done"
   }
 
   data = {
     listAllFeeder: listAllFeeder,
     flash: ctx.flash
   }
-  await ctx.render('edit_form', data)
+  await ctx.render("edit_form", data)
 }
 
-const scrapHandler = async (ctx) => {
+const manualPmHandler = async ctx => {
   let data = {}
   const listAllFeeder = await feeders.findAll()
   console.log(listAllFeeder)
 
   ctx.session.flash = {
-    success: 'scrap feeder done'
+    success: "edit feeder done"
   }
 
   data = {
     listAllFeeder: listAllFeeder,
     flash: ctx.flash
   }
-  await ctx.render('scrap_form', data)
+  await ctx.render("manual_pm_form", data)
+}
+
+const scrapHandler = async ctx => {
+  let data = {}
+  const listAllFeeder = await feeders.findAll()
+  console.log(listAllFeeder)
+
+  ctx.session.flash = {
+    success: "scrap feeder done"
+  }
+
+  data = {
+    listAllFeeder: listAllFeeder,
+    flash: ctx.flash
+  }
+  await ctx.render("scrap_form", data)
 }
 
 const addFeeders = async ctx => {
-
   console.log("addFeeders")
   console.log(ctx.request.body)
   const pm_group = assignpmGroup()
   console.log("pmGroup= " + pm_group)
   const user = ctx.session.username
-  console.log("User is : "+ user)
+  console.log("User is : " + user)
   const creater = user
   const updater = user
-  const { feederId, mfgToolingId, toolingName, model, brand, size, location, status } = ctx.request.body
+  const {
+    feederId,
+    mfgToolingId,
+    toolingName,
+    model,
+    brand,
+    size,
+    location,
+    status
+  } = ctx.request.body
   try {
-    await feeders.addFeeders(feederId, mfgToolingId, toolingName, model, brand, size, location, status, pm_group, creater, updater )
+    await feeders.addFeeders(
+      feederId,
+      mfgToolingId,
+      toolingName,
+      model,
+      brand,
+      size,
+      location,
+      status,
+      pm_group,
+      creater,
+      updater
+    )
     console.log("Insert new feeder complete")
     ctx.status = 200
     if (ctx.status === 200) {
       ctx.session.flash = {
-        success: 'add feeder ' + feederId + ' done'
+        success: "add feeder " + feederId + " done"
       }
-      await ctx.redirect('/form')
+      await ctx.redirect("/form")
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err)
     ctx.session.flash = {
-      error: 'fail to add new feeder => duplicate feeder ID'
+      error: "fail to add new feeder => duplicate feeder ID"
     }
-    await ctx.redirect('/form')
+    await ctx.redirect("/form")
   }
 }
 
 const editFeeders = async ctx => {
-
   console.log("editFeeders")
   const user = ctx.session.username
   const updater = user
-  const { feederId, mfgToolingId, toolingName, brand, model, size, location } = ctx.request.body
+  const {
+    feederId,
+    mfgToolingId,
+    toolingName,
+    brand,
+    model,
+    size,
+    location
+  } = ctx.request.body
   try {
-    feeders.updateFeeders(feederId, mfgToolingId, toolingName, brand, model, size, location, updater)
+    feeders.updateFeeders(
+      feederId,
+      mfgToolingId,
+      toolingName,
+      brand,
+      model,
+      size,
+      location,
+      updater
+    )
     console.log("Update new data complete")
     ctx.status = 200
     if (ctx.status === 200) {
       ctx.session.flash = {
-        success: 'edit feeder done'
+        success: "edit feeder done"
       }
-      await ctx.redirect('/edit_form')
+      await ctx.redirect("/edit_form")
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err)
     ctx.status = 404
     ctx.session.flash = {
-      error: 'fail to edit feeder!'
+      error: "fail to edit feeder!"
     }
-    await ctx.redirect('/edit_form')
+    await ctx.redirect("/edit_form")
   }
 }
 
 const scrapFeeders = async ctx => {
-
   console.log("scrapFeeders")
   const user = ctx.session.username
   const updater = user
   const { feederId } = ctx.request.body
-  const status = 'Scrap'
+  const status = "Scrap"
   try {
     feeders.scrapFeeders(feederId, updater, status)
     console.log("Update new data complete")
     ctx.status = 200
     if (ctx.status === 200) {
       ctx.session.flash = {
-        success: 'scrap feeder done'
+        success: "scrap feeder done"
       }
-      await ctx.redirect('/scrap_form')
+      await ctx.redirect("/scrap_form")
     }
-  }
-  catch (err) {
+  } catch (err) {
     console.error(err)
     ctx.status = 404
     ctx.session.flash = {
-      error: 'fail to scrap feeder!'
+      error: "fail to scrap feeder!"
     }
-    await ctx.redirect('/scrap_form')
+    await ctx.redirect("/scrap_form")
   }
 }
 
-const assignpmGroup = (group) => {
+const assignpmGroup = group => {
   const d = new Date()
   const currentMonth = d.getMonth() + 1
   if ([1, 2, 3].includes(currentMonth)) {
-    return group = 1
+    return (group = 1)
   } else if ([4, 5, 6].includes(currentMonth)) {
-    return group = 2
+    return (group = 2)
   } else if ([7, 8, 9].includes(currentMonth)) {
-    return group = 3
+    return (group = 3)
   } else {
-    return group = 4
+    return (group = 4)
   }
 }
 
@@ -279,5 +329,6 @@ module.exports = {
   scrapHandler,
   addFeeders,
   editFeeders,
+  manualPmHandler,
   scrapFeeders
 }
--- src/route/controller/feedermanage/index.js
+++ src/route/controller/feedermanage/index.js
@@ -1,49 +1,47 @@
-const Router = require('koa-router')
-const feeders = require('./feeders')
-const index2 = require('./index2')
-const index3 = require('./index3')
-const testapi = require('./testapi')
-const pm = require('./pm')
-
-
+const Router = require("koa-router")
+const feeders = require("./feeders")
+const index2 = require("./index2")
+const index3 = require("./index3")
+const testapi = require("./testapi")
+const pm = require("./pm")
 
 const router = new Router()
-
 
 const checkAuth = async (ctx, next) => {
   console.log(ctx.session.username)
   if (ctx.session && ctx.session.username) {
-    console.log('If checkAuth', ctx.session.username)
+    console.log("If checkAuth", ctx.session.username)
     await next()
   } else {
-    console.log('Else checkAuth', ctx.session.username)
-    return ctx.redirect('/')
+    console.log("Else checkAuth", ctx.session.username)
+    return ctx.redirect("/")
   }
 }
 
-router.get('/index', checkAuth, feeders.getHandler)
+router.get("/index", checkAuth, feeders.getHandler)
 
-router.get('/index2', checkAuth, index2.getHandler)
+router.get("/index2", checkAuth, index2.getHandler)
 
-router.get('/index3', checkAuth, index3.getHandler)
+router.get("/index3", checkAuth, index3.getHandler)
 
-router.get('/feederdata', checkAuth, feeders.getData)
+router.get("/feederdata", checkAuth, feeders.getData)
 
-
-router.get('/chartjs', async ctx => {
-  await ctx.render('chartjs')
+router.get("/chartjs", async ctx => {
+  await ctx.render("chartjs")
 })
 
-router.get('/form', checkAuth, feeders.findlastTenRows)
-router.get('/edit_form', checkAuth, feeders.editHandler)
-router.get('/scrap_form', checkAuth, feeders.scrapHandler)
-router.post('/insert_feeders', checkAuth, feeders.addFeeders)
-router.post('/edit_feeders', checkAuth, feeders.editFeeders)
-router.post('/scrap_feeders', checkAuth, feeders.scrapFeeders)
-router.get('/tables', feeders.findAllFeeder)
-router.get('/tables_dynamic', checkAuth, feeders.listAllFeederDue)
-router.post('/testapi', testapi.log)
-router.post('/update_pm', pm.postHandler)
+router.get("/form", checkAuth, feeders.findlastTenRows)
+router.get("/edit_form", checkAuth, feeders.editHandler)
+router.get("/manual_pm_form", checkAuth, feeders.manualPmHandler)
+router.get("/scrap_form", checkAuth, feeders.scrapHandler)
+router.post("/insert_feeders", checkAuth, feeders.addFeeders)
+router.post("/edit_feeders", checkAuth, feeders.editFeeders)
+router.post("/scrap_feeders", checkAuth, feeders.scrapFeeders)
+router.post("/manual_pm_feeders", checkAuth, feeders.manualPmHandler)
+router.get("/tables", feeders.findAllFeeder)
+router.get("/tables_dynamic", checkAuth, feeders.listAllFeederDue)
+router.post("/testapi", testapi.log)
+router.post("/update_pm", pm.postHandler)
 /**
  * the last route get * and render 404error pasge
  */
